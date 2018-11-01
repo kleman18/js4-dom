4fa68c17bb56949f36bc7bd565dcd4575698be7b
Volodymyr Kleman <v.kleman1996@gmail.com>
2018-11-01 22:37:30 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const link = document.querySelector('a');
+link.textContent = 'student';
+
+
+const sect = document.querySelector('section');
+
+for (let i = 0; i < 100; i++) {
+    const para = document.createElement('p');
+    para.textContent = i + 1 + ':You have won the prize!';
+    sect.appendChild(para);
+    para.classList.add('highlight');
+}
+
+const img = document.querySelector('img');
+sect.appendChild(img);
+
+const h1 = document.querySelector('h1');
+h1.parentNode.removeChild(h1);
+
+
+$('.hide-pars').click(hidePars);
+function hidePars() {
+    $('p').hide();
+}
+
+document.querySelectorAll('.digits')
+.forEach( el => el.addEventListener('click', digitClick ) );
+
+function digitClick(e) {
+    const target = e.target;
+    const display = document.querySelector('.display');
+    display.value += target.innerText;
+}
+
